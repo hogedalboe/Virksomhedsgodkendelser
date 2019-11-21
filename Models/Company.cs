fc99a7a4bd75b2cbf840dc701f459004fdd1b534
Nikolaj Boe <hogedalboe@outlook.com>
2019-11-21 18:33:40 +0100

    API finished.

--- Models/Company.cs
+++ Models/Company.cs
@@ -15,45 +15,8 @@ namespace Virksomhedsgodkendelser.Models
         public int Pnr { get; set; }
 
         public string Virksomhedsnavn { get; set; }
-
         
         [Display(Name = "CVR-nr")]
         public string CVRnr { get; set; }
-
-
-
-        [Display(Name = "Postnummer og by")]
-        public string PostnrPostdistrikt { get; set; }
-
-        [Display(Name = "Telefonnummer")]
-        public string Tlfnr { get; set; }
-
-        [Display(Name = "Uddannelse")]
-        public string UddannelseskodeUddannelsesbetegnelse { get; set; }
-
-        [Display(Name = "CØSA")]
-        public string COSAnr { get; set; }
-
-        [Display(Name = "Speciale")]
-        public string SpecialekodeSpecialebetegnelse { get; set; }
-
-        [Display(Name = "Godkendelse oprettet")]
-        public DateTime GodkendelseOprettet { get; set; }
-
-        [Display(Name = "Nygodkendelse")]
-        public int Nygodkendelse { get; set; }
-
-        [Display(Name = "Godkendelsesdato")]
-        public DateTime Godkendelsesdato { get; set; }
-
-        [Display(Name = "Godkendelsesændring")]
-        public int Godkendelsesaendring { get; set; }
-
-        [Display(Name = "Godkendelsesantal")]
-        public int Godkendelsesantal { get; set; }
-
-        [Display(Name = "Antal igangværende aftaler")]
-        public int AntalIgangvarendeAftaler { get; set; }
-        
     }
 }
