5e13be47995543b2de43c943d07235e44634aa7c
Nikolaj Boe <hogedalboe@outlook.com>
2019-11-25 16:11:28 +0100

    Styling, url query parameters, general app structure.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -4,21 +4,36 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Virksomhedsgodkendelser.Models;
 
 namespace Virksomhedsgodkendelser.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly Data.VirksomhedsgodkendelserContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IList<Company> Company { get; set; }
+
+        public IndexModel(Data.VirksomhedsgodkendelserContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+        public string SearchParam { get; set; }
+
+        public async Task OnGetAsync(int pageindex = 1, int pagesize = 50, string search = null)
         {
+            // Set filters: ~?pageindex=3&pagesize=10&search=novo-nordisk&
+            PageIndex = pageindex;
+            PageSize = pagesize;
+            SearchParam = search;
+
+            // Return only the page-delimited interval of companies
+            Company = await _context.Company.ToListAsync();
 
         }
     }
