e6a747d8f873223eead97f82c391fed8578a9191
Nikolaj Høgedal Boe <45713352+hogedalboe@users.noreply.github.com>
2019-11-26 23:44:42 +0100

    Added models.

---
+++ Models/Approval.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Virksomhedsgodkendelser.Models
+{
+    public class Approval
+    {
+        public int ID { get; set; }
+        public int CompanyID { get; set; }
+        public int SpecialisationID { get; set; }
+
+    }
+}
