904891700cfc4ad744fbddefcb7c3cad1646c771
Nikolaj Boe <hogedalboe@outlook.com>
2019-11-26 12:36:07 +0100

    Pagination and other cool stuff.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -16,9 +16,10 @@ namespace Virksomhedsgodkendelser.Pages
 
         public IList<Company> Company { get; set; }
 
-        // Pageing
+        // Paging
         public int PageIndex { get; set; }
         public int PageSize { get; set; }
+        public int[] PageSizes = new int[5] { 20, 50, 100, 150, 200 };
         public int PageCount { get; set; }
 
         // Filter and sorting parameters
@@ -33,7 +34,7 @@ namespace Virksomhedsgodkendelser.Pages
 
         // Focus
         public string CompanyFocus { get; set; } // Use the API to fetch data about individual companies, instead of filling the browser with all 50 of them?
-
+        public int CompanyCount { get; set; }
 
         public IndexModel(Data.VirksomhedsgodkendelserContext context)
         {
@@ -42,16 +43,29 @@ namespace Virksomhedsgodkendelser.Pages
 
         public async Task OnGetAsync(int pageindex = 1, int pagesize = 50, string search = "")
         {
-            // Set parametres: ~?pageindex=3&pagesize=10&search=novo-nordisk&
-            PageIndex = pageindex;
-            PageSize = pagesize;
-            SearchParam = search;
-
             // Return only the page-delimited interval of companies
             Company = await _context.Company.ToListAsync();
 
-            // Set pages -----------------------------------------------------------------> TEST: Antal sider skal beregnes ud fra Company
+            // Determine page and row count from data
             PageCount = 10;
+            CompanyCount = 234;
+
+            // Set parametres: ~?pageindex=3&pagesize=10&search=novo-nordisk&
+
+            // Page range
+            if (pageindex > 0 && pageindex <= PageCount)
+            {
+                PageIndex = pageindex;
+            }
+            else
+            {
+                PageIndex = 1;
+            }
+            
+            // Page size
+
+            PageSize = pagesize;
+            SearchParam = search;
         }
     }
 }
